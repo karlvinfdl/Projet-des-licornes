d04c470956d846b431e6c5304858f82822a396ce
Alexis <bailalexis59@gmail.com>
2026-03-03 15:13:27 +0100

    selection de personnage page equipe app.js)

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,61 @@
 import './stimulus_bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 
+/* ===================================== */
+/* ============== LANDING DEBUT========= */
+/* ===================================== */
+
+/* ===================================== */
+/* ============== LANDING FIN=========== */
+/* ===================================== */
+
+/* ===================================== */
+/* =============== EQUIPE DEBUT========= */
+/* ===================================== */
+// Si tu décides de mettre la logique ici plutôt que dans le Twig :
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Fonction de sélection des membres
+    const handleMemberSelection = () => {
+        const members = document.querySelectorAll('.js-member');
+        
+        members.forEach(m => {
+            m.addEventListener('click', () => {
+                // On enlève la classe leader à tout le monde
+                members.forEach(member => member.classList.remove('leader'));
+                // On l'ajoute au membre cliqué
+                m.classList.add('leader');
+            });
+        });
+    };
+
+    handleMemberSelection();
+    console.log('Système de sélection de personnage actif ! 🕹️');
+});
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+/* ===================================== */
+/* =============== EQUIPE FIN=========== */
+/* ===================================== */
+
+/* ===================================== */
+/* =============== POPUP DEBUT========== */
+/* ===================================== */
+
+/* ===================================== */
+/* =============== POPUP FIN============ */
+/* ===================================== */
+
+/* ===================================== */
+/* ================= FIN DEBUT========== */
+/* ===================================== */
+
+/* ===================================== */
+/* ================= FIN FIN============ */
+/* ===================================== */
+
+/* ===================================== */
+/* ===== RESPONSIVE DEBUT=============== */
+/* ===================================== */
+
+/* ===================================== */
+/* ======= RESPONSIVE FIN=============== */
+/* ===================================== */
