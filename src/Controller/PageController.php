d7298b66c6f60a014ad51fce649c6a40af01aebe
karlvin <karlvinfidelin@email.com>
2026-03-31 12:10:11 +0200

    `Updated routes, controllers, and templates for new features and functionality`

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,39 +8,57 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    // =========================
-    // Home - Redirect to landing
-    // =========================
     #[Route('/', name: 'home')]
     public function home(): Response
     {
         return $this->redirectToRoute('landing');
     }
 
-    // =========================
-    // Landing
-    // =========================
     #[Route('/landing', name: 'landing')]
     public function landing(): Response
     {
         return $this->render('page/landing.html.twig');
     }
 
-    // =========================
-    // Equipe
-    // =========================
     #[Route('/equipe', name: 'equipe')]
     public function equipe(): Response
     {
         return $this->render('page/equipe.html.twig');
     }
 
-    // =========================
-    // Fin
-    // =========================
     #[Route('/fin', name: 'fin')]
     public function fin(): Response
     {
         return $this->render('page/fin.html.twig');
+    }
+
+    #[Route('/wheel', name: 'wheel')]
+    public function wheel(): Response
+    {
+        return $this->render('page/wheel.html.twig');
+    }
+
+    #[Route('/devinelemot', name: 'devinelemot')]
+    public function devinelemot(): Response
+    {
+        return $this->render('page/devinelemot.html.twig');
+    }
+
+    #[Route('/pinata', name: 'pinata')]
+    public function pinata(): Response
+    {
+        return $this->render('page/pinata.html.twig');
+    }
+
+    #[Route('/scrabble', name: 'scrabble')]
+    public function scrabble(): Response
+    {
+        return $this->render('page/scrabble.html.twig');
+    }
+
+    #[Route('/coaster', name: 'coaster')]
+    public function coaster(): Response
+    {
+        return $this->render('page/coaster.html.twig');
     }
 }
